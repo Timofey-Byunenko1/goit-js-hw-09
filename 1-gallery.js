e9fcc05e586240371c5fa2c5e635f365284f7648
Timofey-Byunenko1 <byunenkotimofey@gmail.com>
2024-11-19 14:24:33 +0000

    Deploying to gh-pages from @ Timofey-Byunenko1/goit-js-hw-09@32cbd8db28cb369ce8b6b752b3c44ae2e2af5cda 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as l}from"./assets/vendor-CgTBfC_f.js";const t=[{preview:"path-to-preview-image1.jpg",original:"path-to-original-image1.jpg",description:"Image 1 Description"},{preview:"path-to-preview-image2.jpg",original:"path-to-original-image2.jpg",description:"Image 2 Description"}],o=document.querySelector(".gallery"),g=a=>a.map(({preview:e,original:i,description:r})=>`
+import"./assets/styles-B4s-4mBC.js";import{S as l}from"./assets/vendor-CgTBfC_f.js";const t=[{preview:"path-to-preview-image1.jpg",original:"path-to-original-image1.jpg",description:"Image 1 Description"},{preview:"path-to-preview-image2.jpg",original:"path-to-original-image2.jpg",description:"Image 2 Description"}],o=document.querySelector(".gallery"),g=a=>a.map(({preview:e,original:i,description:r})=>`
       <li class="gallery-item">
         <a class="gallery-link" href="${i}">
           <img
